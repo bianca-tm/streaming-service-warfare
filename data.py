874fd2250423b257f90c01dfd2cf3aca87d369b1
bianca-tm <tech.btovalin@gmail.com>
2024-07-28 15:56:25 -0400

    imports and TODOs

---
+++ data.py
@@ -0,0 +1,10 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing
+
+import plotly
+import plotly.express as px
+from plotly.subplots import make_subplots
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# TODO: import data and clean/prepare it
